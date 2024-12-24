c4772d80d86a90f2ec97a5713062ba663f274ecc
Adjel van der Meer <eavdmeer@gmail.com>
2024-12-24 23:59:53 +0100

    Non-working part2 code

--- day24/index.js
+++ day24/index.js
@@ -13,6 +13,18 @@ if (process.argv[2])
 
 const pad = num => String(num).padStart(2, '0');
 
+const tobits = dec => dec.toString(2);
+
+function intersect(set, otherSet)
+{
+  const result = new Set();
+  otherSet.forEach(value =>
+  {
+    if (set.has(value)) { result.add(value); }
+  });
+  return result;
+}
+
 const f = {
   OR: (a, b) => a || b,
   AND: (a, b) => a && b,
@@ -27,10 +39,9 @@ function evaluate(a1, a2, op, logic, init)
   );
 }
 
-function solve1(data)
+function solve(input, logic)
 {
-  debug('data:', data);
-  const bits = 1n + BigInt([ ...data.logic.keys() ]
+  const bits = 1n + BigInt([ ...logic.keys() ]
     .filter(v => v.startsWith('z'))
     .toSorted()
     .at(-1)
@@ -39,7 +50,7 @@ function solve1(data)
   let result = 0n;
   for (let bit = 0n; bit < bits; bit++)
   {
-    if (evaluate(...data.logic.get(`z${pad(bit)}`), data.logic, data.initial))
+    if (evaluate(...logic.get(`z${pad(bit)}`), logic, input))
     {
       result |= 1n << bit;
     }
@@ -48,10 +59,116 @@ function solve1(data)
   return result;
 }
 
+function solve1(data)
+{
+  debug('data:', data);
+
+  return solve(data.initial, data.logic);
+}
+
+function expand(wire, logic)
+{
+  const [ a1, a2, op ] = logic.get(wire);
+  return [
+    /^[xy]/.test(a1) ? a1 : expand(a1, logic),
+    /^[xy]/.test(a2) ? a2 : expand(a2, logic),
+    op
+  ];
+}
+
 function solve2(data)
 {
   debug('data:', data);
-  return 0;
+
+  const all = [ ...data.logic.entries() ];
+
+  const res = new Set();
+  const bad = {};
+
+  const bits = 1n + BigInt([ ...data.logic.keys() ]
+    .filter(v => v.startsWith('z'))
+    .toSorted()
+    .at(-1)
+    .substr(1));
+
+  for (let bit = 0; bit < bits; bit++)
+  {
+    console.log(JSON.stringify(expand(`z${pad(bit)}`, data.logic)));
+  }
+
+  const lastBit = `z${pad(bits - 1n)}`;
+
+  // Outputs must be the result of an XOR.
+  // Only exception is the last bit, which is the carry bit from the last two
+  // bits being added. This is an OR
+  bad.output = all
+    .filter(([ o, [ , , op ] ]) =>
+    {
+      if (/^z/.test(o))
+      {
+        if (op === 'OR' && o === lastBit) { return false; }
+        if (op !== 'XOR') { return true; }
+        return false;
+      }
+      return false;
+    })
+    .map(v => v[0]);
+  bad.output.forEach(v => res.add(v));
+  console.log('zxx output not the result of an XOR');
+  console.log(bad.output);
+
+  // AND output not connected to an OR
+  // Only exception is z01, where it can be connected to an XOR due to
+  // carry bit input for z00 being 0
+  bad.and = all
+    .filter(([ , [ , , op ] ]) => op === 'AND')
+    .map(v => v[0])
+    .filter(v =>
+    {
+      const gotor = all.some(([ o, [ a, b, op ] ]) =>
+        (op === 'OR' || op === 'XOR' && o === 'z01') &&
+        (a === v || b === v));
+      return ! gotor;
+    });
+  bad.and.forEach(v => res.add(v));
+  console.log('AND output not connected to an OR');
+  console.log(bad.and);
+
+  // OR output not connected to XOR + AND
+  bad.or = all
+    .filter(([ o, [ , , op ] ]) => op === 'OR' && ! /^z/.test(o))
+    .map(v => v[0])
+    .filter(v =>
+    {
+      const gotand = all.some(([ , [ a, b, op ] ]) =>
+        op === 'AND' && (a === v || b === v));
+      const gotxor = all.some(([ , [ a, b, op ] ]) =>
+        op === 'XOR' && (a === v || b === v));
+      return ! gotand || !gotxor;
+    })
+    .filter(v => v[0] !== 'z' || data.logic.get(v)[2] !== 'XOR');
+  bad.or.forEach(v => res.add(v));
+  console.log('OR output not connected to XOR + AND');
+  console.log(bad.or);
+
+  // XOR output not connected to XOR + AND
+  bad.xor = all
+    .filter(([ o, [ , , op ] ]) => op === 'XOR' && ! /^z/.test(o))
+    .map(v => v[0])
+    .filter(v =>
+    {
+      const gotand = all.some(([ , [ a, b, op ] ]) =>
+        op === 'AND' && (a === v || b === v));
+      const gotxor = all.some(([ , [ a, b, op ] ]) =>
+        op === 'XOR' && (a === v || b === v));
+      return ! gotand || !gotxor;
+    })
+    .filter(v => v[0] !== 'z' || data.logic.get(v)[2] !== 'XOR');
+  bad.xor.forEach(v => res.add(v));
+  console.log('XOR output not connected to XOR + AND');
+  console.log(bad.xor);
+
+  return [ ...res ].sort().join(',');
 }
 
 export default async function day24(target)
@@ -91,7 +208,7 @@ export default async function day24(target)
   {
     throw new Error(`Invalid part 2 solution: ${part2}. Expecting; ${expect2a}`);
   }
-  const expect2b = 0;
+  const expect2b = 'cvp,mkk,qbw,wcb,wjb,z10,z14,z34';
   if (target === 'data.txt' && part2 !== expect2b)
   {
     throw new Error(`Invalid part 2 solution: ${part2}. Expecting; ${expect2b}`);
